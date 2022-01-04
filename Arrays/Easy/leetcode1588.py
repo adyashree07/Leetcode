f8ccc353fc76db7e0b772b951ead3d6fcdb8a634
Adyashree Mahapatra <adyashree.mahapa@tigeranalytics.com>
2022-01-04 16:14:13 +0530

    1588. Sum of All Odd Length Subarrays

--- Arrays/Easy/leetcode1588.py
+++ Arrays/Easy/leetcode1588.py
@@ -1,3 +1,4 @@
+#My submitted solution
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         prefix_sum = [0]*len(arr)
@@ -20,3 +21,12 @@ class Solution:
         sum+=prefix_sum[0]
                     
         return sum
+    
+    
+    #Optimal Solution
+    class Solution:
+    def sumOddLengthSubarrays(self, A: List[int]) -> int:
+        ans, n = 0, len(A)
+        for i, v in enumerate(A):
+            ans += ((i+1) * (n-i) + 1) // 2 * A[i]
+        return ans
