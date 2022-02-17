81627b4412a8d5af4a59436817a6b9fce5210865
Adyashree Mahapatra <32678922+adyashree07@users.noreply.github.com>
2022-02-17 14:55:02 +0530

    176. Second Highest Salary

--- Database/Medium/leetcode0176.sql
+++ Database/Medium/leetcode0176.sql
@@ -1,10 +1,22 @@
 # https://leetcode.com/problems/second-highest-salary/
 
+# Table: Employee
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | id          | int  |
+# | salary      | int  |
+# +-------------+------+
+# id is the primary key column for this table.
+# Each row of this table contains information about the salary of an employee.
+ 
+# Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
 # My Solution
 SELECT (SELECT DISTINCT SALARY AS SECONDHIGHESTSALARY FROM EMPLOYEE ORDER BY SALARY DESC limit 1,1) AS SECONDHIGHESTSALARY;
 
 
-# Optimal Solution
+# Alternate Solution
 SELECT
     IFNULL(
       (SELECT DISTINCT Salary
@@ -12,3 +24,10 @@ SELECT
        ORDER BY Salary DESC
         LIMIT 1 OFFSET 1),
     NULL) AS SecondHighestSalary;
+    
+    
+# Alternate Solution-2
+SELECT Max(Salary) as  SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary) FROM Employee );
+
