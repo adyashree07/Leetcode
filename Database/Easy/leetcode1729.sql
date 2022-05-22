1c205b28c48f6eb6a73cc17bfb82aaa87158a8da
Adyashree Mahapatra <32678922+adyashree07@users.noreply.github.com>
2022-05-22 15:55:53 +0530

    1729. Find Followers Count

---
+++ Database/Easy/leetcode1729.sql
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/find-followers-count/
+
+# Table: Followers
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | user_id     | int  |
+# | follower_id | int  |
+# +-------------+------+
+# (user_id, follower_id) is the primary key for this table.
+# This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+ 
+# Write an SQL query that will, for each user, return the number of followers.
+# Return the result table ordered by user_id.
+
+# My Solution
+SELECT USER_ID,
+       COUNT(FOLLOWER_ID) AS FOLLOWERS_COUNT
+FROM FOLLOWERS
+GROUP BY 1
+ORDER BY 1;
+
